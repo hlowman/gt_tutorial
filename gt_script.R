da887efe33376f345eeea9ca2b5194398f25863b
Heili Lowman <heilil@sccwrp.org>
2020-12-01 12:59:50 -0800

    Created and exported pizza_table.

--- gt_script.R
+++ gt_script.R
@@ -33,3 +33,65 @@ pizza_money <- pizza %>% # Take the original dataset
     levels = c("classic", "veggie", "chicken", "supreme"))) %>% # Releveling pizza types
   summarize(sold = n(), profit = sum(price)) %>% # Summarize profits by defined groups
   ungroup() # Always make sure to ungroup after you group!!
+
+# Build our table below
+
+pizza_table <- pizza_money %>%
+  
+  # Make some edits to underlying dataset.
+  filter(month_num > 9) %>% # Filter for Q4
+  
+  mutate(month_name = case_when(month_num == 10 ~ "October",
+    month_num == 11 ~ "November",
+    month_num == 12 ~ "December")) %>% # Create month names
+  
+  gt(groupname_col = "month_name", rowname_col = "type") %>% # Base table creation/call
+  
+  # Format our table
+  cols_hide(columns = vars(month_num)) %>% # Hide column
+  fmt_currency(columns = vars(profit), currency = "USD") %>% # Format profit into dollars
+  
+  # Add helpful titles
+  tab_header(title = "Q4 Monthly Pizza Sales",
+    subtitle = "by Heili Lowman") %>% # Adds title & subtitle
+  tab_source_note(md("More information can be found at `?pizzaplace`.")) %>% # Adds a nice footer.
+
+  # Add helpful column names.
+  cols_label(sold = "Pizzas Sold",
+    profit = "Profits") %>%
+  tab_stubhead(label = "Month") %>% # Renames stubhead
+  tab_spanner(label = "Pizzas Sold + Revenue",
+    columns = vars(sold, profit)) %>% # Adds spanner
+  
+  # Add more summary statistics.
+  summary_rows(groups = TRUE,
+    columns = vars(profit),
+    fns = list("TOTAL" = "sum"),
+    formatter = fmt_currency, currency = "USD",
+    use_seps = TRUE) %>% # Monthly summary rows
+  grand_summary_rows(
+    columns = vars(profit),
+    fns = list("GRAND TOTAL" = "sum"),
+    formatter = fmt_currency, currency = "USD",
+    use_seps = TRUE) %>% # Grand summary row
+  
+  # Add some color.
+  data_color(
+    columns = vars(profit),
+    colors = scales::col_numeric(
+      palette = paletteer::paletteer_d(
+        palette = "ggsci::teal_material") %>%
+        as.character(),
+      domain = NULL),
+      alpha = 0.75)
+
+pizza_table
+
+# Save our table.
+gtsave(pizza_table,
+  "pizza_table.png",
+  path = "/Users/heilil/Desktop/R_figures")
+
+# End of script.
+
+
